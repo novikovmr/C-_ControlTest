06346aee1b9d9423a3a884fd085155ee591ad03b
Max <maksim101101@gmail.com>
2023-08-30 20:04:52 +0400

    Добавлена функция на размер матрицы

--- Program.cs
+++ Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*
+Написать программу, которая из имеющегося массива строк формирует новый массив из строк, длина которых меньше, либо равна 3 символам. 
+Первоначальный массив можно ввести с клавиатуры, либо задать на старте выполнения алгоритма. При решении не рекомендуется пользоваться коллекциями, 
+лучше обойтись исключительно массивами.
+Примеры:
+[“Hello”, “2”, “world”, “:-)”] → [“2”, “:-)”]
+[“1234”, “1567”, “-2”, “computer science”] → [“-2”]
+[“Russia”, “Denmark”, “Kazan”] → []
+*/
+
+Console.Clear();
+
+int InputSize (string msg)
+{
+    Console.Write(msg);
+    int sizeArray = Convert.ToInt32(Console.ReadLine());
+    return sizeArray;
+}
+
+void InputArray (string[] stringArray)
+{
+    
+}
+
+
+int sizeArray = InputSize("Введите размер массива: ");
